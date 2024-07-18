6a01fa66bcca4b114802ff465df1f13919f59fa5
cristianesilvanunes <eebmp.csn@educacao.arcos.mg.gov.br>
2024-07-17 22:28:19 -0300

    18/07/2024

---
+++ TestarMetodo.java
@@ -0,0 +1,10 @@
+    public class TestarMetodo{
+        public static void main(String[] args){
+            String nome = "Cris";
+            imprimirNome(nome);
+
+        }
+        public static void imprimirNome(String nome){
+            System.out.println("Bem-vindo," + nome);
+    }
+}
