a8ef1539d11d32cd098ef46e5fa4dfe8b548f5fd
cristianesilvanunes <eebmp.csn@educacao.arcos.mg.gov.br>
2024-08-20 22:12:04 -0300

    20/08/24

---
+++ Gerencia.java
@@ -0,0 +1,8 @@
+public class Gerencia {
+ public static void main(String[] args) {
+    String setor = "Gerencia";
+    Producao producao = new Producao ();
+    System.out.println(setor);
+    producao.verSetor();
+ }   
+}
---
+++ Producao.java
@@ -0,0 +1,12 @@
+public class Producao {
+    String setor = "Produção";
+    public void verSetor(){
+        System.out.println("Você está no setor de produção: ");
+   }  
+    private static void verStatus(){
+        System.out.println("Este método é privado");
+   }
+    public static void main (String[]args){
+    verStatus();
+   }
+}
